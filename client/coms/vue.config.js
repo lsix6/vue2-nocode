407240e1a29d8730422e7fbcbe4fc053812f7ea2
2242938055 <2242938055@qq.com>
2023-04-11 16:36:22 +0800

    vue.config.js exclude ‘vue’

--- client/coms/vue.config.js
+++ client/coms/vue.config.js
@@ -3,6 +3,11 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/lib/coms/',
   outputDir: './dist/lib/coms/',
+  configureWebpack: (config) => {
+    config.externals = {
+      'vue': 'Vue',
+    };
+  },
   devServer: {
     port: 8200,
   },
