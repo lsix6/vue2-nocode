7b620f6f1762f9d10cbef3b7a59b0c64827357a6
2242938055 <2242938055@qq.com>
2023-04-11 16:41:45 +0800

    main.js register coms

--- client/coms/src/main.js
+++ client/coms/src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+console.log('[coms] main.js')
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const registerComponents = (Vue) => {
+  console.log('[coms] register components')
+  Vue.component('HelloWorld', () => import('./components/HelloWorld.vue'))
+}
+
+if (window.Vue) {
+  // 主应用中赋值了 window.Vue
+  // 远程调用
+  registerComponents(window.Vue)
+} else {
+  // 本地调试
+  Promise.all([
+    import('vue'),
+    import('./App.vue'),
+  ]).then(([vueModule, appModule]) => {
+    const Vue = vueModule.default
+    const App = appModule.default
+    // console.log('[coms] import', Vue, App)
+    //
+    registerComponents(Vue)
+    //
+    Vue.config.productionTip = false
+
+    new Vue({
+      render: h => h(App),
+    }).$mount('#app')
+  })
+
+}
