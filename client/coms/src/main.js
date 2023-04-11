2c8e934dbd1d54e807e4df3c1e86f2f34288341b
2242938055 <2242938055@qq.com>
2023-04-11 17:10:39 +0800

    coms/main.js edit

--- client/coms/src/main.js
+++ client/coms/src/main.js
@@ -1,32 +1,15 @@
 
-console.log('[coms] main.js')
+window.time_log('[coms] main.js')
 
 const registerComponents = (Vue) => {
-  console.log('[coms] register components')
+  window.time_log('[coms] register components')
   Vue.component('HelloWorld', () => import('./components/HelloWorld.vue'))
 }
 
 if (window.Vue) {
   // 主应用中赋值了 window.Vue
-  // 远程调用
+  // 注册组件
   registerComponents(window.Vue)
 } else {
-  // 本地调试
-  Promise.all([
-    import('vue'),
-    import('./App.vue'),
-  ]).then(([vueModule, appModule]) => {
-    const Vue = vueModule.default
-    const App = appModule.default
-    // console.log('[coms] import', Vue, App)
-    //
-    registerComponents(Vue)
-    //
-    Vue.config.productionTip = false
-
-    new Vue({
-      render: h => h(App),
-    }).$mount('#app')
-  })
-
+  console.error(`not found 'window.Vue'`)
 }
