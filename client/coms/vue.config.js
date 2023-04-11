f10c408dc1c22c618bf941215ae910640726366a
2242938055 <2242938055@qq.com>
2023-04-11 16:34:22 +0800

    vue.config.js set ‘publicPath’ & ‘outputDir’

--- client/coms/vue.config.js
+++ client/coms/vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/lib/coms/',
+  outputDir: './dist/lib/coms/',
   devServer: {
     port: 8200,
   },
