f7a8ef4f994afe8f1e384caa55306f48e4071343
2242938055 <2242938055@qq.com>
2023-04-12 16:48:30 +0800

    nocode/vue.config.js edit

--- client/nocode/vue.config.js
+++ client/nocode/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/lib/nocode/',
+  outputDir: './dist/lib/nocode/',
+  configureWebpack: (config) => {
+    config.externals = {
+      'vue': 'Vue',
+    };
+  },
+  devServer: {
+    port: 8100,
+  },
 })
