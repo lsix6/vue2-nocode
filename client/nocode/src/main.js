56c249c45c521089685c1b697cdee558bb0a44fc
2242938055 <2242938055@qq.com>
2023-04-12 16:53:30 +0800

    nocde/main.js register components

--- client/nocode/src/main.js
+++ client/nocode/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+window.time_log('[nocode] main.js')
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const registerComponents = (Vue) => {
+  window.time_log('[nocode] register components')
+  Vue.component('NoCode', () => import('./components/HelloWorld.vue'))
+}
+
+if (window.Vue) {
+  // 主应用中赋值了 window.Vue
+  // 注册组件
+  registerComponents(window.Vue)
+} else {
+  console.error(`not found 'window.Vue'`)
+}
