25ae8e3ed3c2b4564a4a786725a2f36e5ecca8aa
2242938055 <2242938055@qq.com>
2023-04-23 13:54:30 +0800

    service.js count page

--- client/nocode/src/test/service.js
+++ client/nocode/src/test/service.js
@@ -13,23 +13,49 @@ let listProducts = [
     },
 ]
 
+const init_products = () => {
+    for (let i = 3; i <= 150; i++) {
+        listProducts.push({
+            no: '' + i,
+            name: '' + i,
+            price: i * 10,
+        })
+    }
+}
+init_products()
+
 const product_list = (params) => {
     console.log('product_list, params', params)
     return new Promise(resolve => {
+        //
         let list = listProducts
         if (params.search && params.search.keyWord) {
             list = listProducts.filter(item => {
                 return item.name.indexOf(params.search.keyWord) >= 0
             })
         }
+        //
+        let pageData = {
+            pageCount: 1,
+            pageNumber: 1,
+            pageSize: 20,
+            totalCount: list.length,
+        }
+        //
+        const page = params.page
+        if (page && page.pageIndex) {
+            pageData.pageNumber = page.pageIndex
+            pageData.pageSize = page.pageSize
+        }
+        pageData.pageCount = Math.floor((pageData.totalCount - 1) / pageData.pageSize) + 1
+        pageData.pageNumber = Math.min(pageData.pageNumber, pageData.pageCount)
+        //
+        const start = (pageData.pageNumber - 1) * pageData.pageSize
+        const end = start + pageData.pageSize
+        const listData = _.cloneDeep(list.slice(start, end))
         const ret = {
-            listData: _.cloneDeep(list),
-            pageData: {
-                pageCount: 1,
-                pageNumber: 1,
-                pageSize: 20,
-                totalCount: listProducts.length,
-            },
+            listData,
+            pageData,
         }
         resolve(ret)
     })
