aefe08215156e069ce25232f4c0c1738c5479b48
2242938055 <2242938055@qq.com>
2023-04-23 17:05:11 +0800

    add ‘nc_form_item.vue’

---
+++ client/coms/src/components/nc_form_item.vue
@@ -0,0 +1,63 @@
+<template>
+    <el-form-item ref="refFormItem" v-bind="form_item_props">
+        <slot></slot>
+    </el-form-item>
+</template>
+
+<script>
+
+export default {
+    install(Vue) {
+        Vue.component('nc_form_item', this)
+    },
+    props: {
+        form_item_props: {
+            type: Object,
+            default: () => { }
+        },
+    },
+    mounted() {
+        // console.log('mounted', this.$refs.refFormItem)
+        this.addFocusListener(this.$refs.refFormItem.$el)
+    },
+    beforeDestroy() {
+        // console.log('beforeDestroy', this.$refs.refFormItem)
+        this.removeFocusListener(this.$refs.refFormItem.$el)
+    },
+    methods: {
+        addFocusListener(node) {
+            if (node) {
+                // console.log('addFocusListener, localName', node.localName)
+                if (node.localName === 'input') {
+                    node.addEventListener('focus', this.onFocus)
+                }
+                if (node.children) {
+                    for (let i = 0; i < node.children.length; i++) {
+                        const child = node.children[i]
+                        this.addFocusListener(child)
+                    }
+                }
+            }
+        },
+        removeFocusListener(node) {
+            if (node) {
+                // console.log('removeFocusListener, localName', node.localName)
+                if (node.localName === 'input') {
+                    node.removeEventListener('focus', this.onFocus)
+                }
+                if (node.children) {
+                    for (let i = 0; i < node.children.length; i++) {
+                        const child = node.children[i]
+                        this.addFocusListener(child)
+                    }
+                }
+            }
+        },
+        onFocus() {
+            // console.log('onFocus')
+            // 输入框获取焦点时移除该表单项的校验结果
+            this.$refs.refFormItem.clearValidate()
+        },
+    },
+}
+</script>
