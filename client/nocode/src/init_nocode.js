04aa52db85dea5bc9b764cd39268b0dd2d6932f3
2242938055 <2242938055@qq.com>
2023-04-23 16:19:43 +0800

    move files => ‘/pages/product/list’

--- client/nocode/src/init_nocode.js
+++ client/nocode/src/init_nocode.js
@@ -5,7 +5,7 @@ Vue.use(nc_component)
 
 
 import { register_page } from "./components/nc_pages"
-import { product_list_page } from './pages/product/product_list_page'
+import { product_list_page } from './pages/product/list/product_list_page'
 import { request_api } from './test/service'
 import { register_request_api } from './utils/nc_request'
 
