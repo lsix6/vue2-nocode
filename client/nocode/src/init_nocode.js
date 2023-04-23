4b0c64b62432e73b210d312ac49a5006676578bd
2242938055 <2242938055@qq.com>
2023-04-23 16:24:11 +0800

    add ‘product_add_page.js’

--- client/nocode/src/init_nocode.js
+++ client/nocode/src/init_nocode.js
@@ -5,6 +5,7 @@ Vue.use(nc_component)
 
 
 import { register_page } from "./components/nc_pages"
+import { product_add_page } from './pages/product/add/product_add_page'
 import { product_list_page } from './pages/product/list/product_list_page'
 import { request_api } from './test/service'
 import { register_request_api } from './utils/nc_request'
@@ -13,4 +14,5 @@ import { register_request_api } from './utils/nc_request'
 export const init_nocode = () => {
     register_request_api(request_api)
     register_page('product/list', product_list_page)
+    register_page('product/add', product_add_page)
 }
---
+++ client/nocode/src/pages/product/add/product_add_page.js
@@ -0,0 +1,18 @@
+
+export const product_add_page = {
+    com_name: "div",
+    com_version: '0',
+    com_props: {
+        style: {
+            background: '#F5F6F7',
+            flex: 1,
+            display: 'flex',
+        },
+    },
+    com_children: [
+        {
+            com_name: 'div',
+            com_text: 'add product',
+        },
+    ],
+}
