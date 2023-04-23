13e940ecd1a7b214a4f84ebaf7ae9631cd0f32ce
2242938055 <2242938055@qq.com>
2023-04-23 17:29:05 +0800

    product_add_page.js edit css

--- client/nocode/src/pages/product/add/product_add_page.js
+++ client/nocode/src/pages/product/add/product_add_page.js
@@ -12,9 +12,44 @@ export const product_add_page = {
     },
     com_children: [
         {
-            com_name: 'div',
-            com_text: 'add product',
+            com_name: 'el-container',
+            com_props: {
+                direction: 'vertical',
+            },
+            com_children: [
+                {
+                    com_name: 'el-header',
+                    com_props: {
+                        style: {
+                            padding: '50px',
+                            'text-align': 'center',
+                        },
+                    },
+                    com_children: [
+                        {
+                            com_name: 'div',
+                            com_text: '添加新产品',
+                        },
+                    ],
+                },
+                {
+                    com_name: 'el-main',
+                    com_children: [
+                        {
+                            com_name: 'div',
+                            com_props: {
+                                style: {
+                                    width: '800px',
+                                    margin: 'auto',
+                                },
+                            },
+                            com_children: {
+                                product_formAdd,
+                            },
+                        },
+                    ],
+                },
+            ],
         },
-        product_formAdd,
     ],
 }
