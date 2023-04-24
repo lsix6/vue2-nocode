1b1cf472adf5843b88c9c3ae53127873e9dd8d15
2242938055 <2242938055@qq.com>
2023-04-24 10:48:40 +0800

    /nocode add ‘nc_commands_request.js’

---
+++ client/nocode/src/commands/nc_commands_request.js
@@ -0,0 +1,26 @@
+import { request_api } from "../utils/nc_request"
+import { get_params } from "../utils/nc_params"
+import { register_commands } from "./nc_commands_base"
+
+const commands = {
+
+    'request_api': async function (com, command) {
+        //
+        return new Promise((resolve, reject) => {
+            const requestParams = get_params(com, command.cmd_params.request_params)
+            request_api(command.cmd_params.api, requestParams).then(data => {
+                console.log(`[nc_commands_request] request_api('${command.cmd_params.api}') return`, data)
+                if (data) {
+                    Object.assign(com.com_params, data)
+                }
+                //
+                resolve(true)
+            }).catch(err => {
+                reject(err)
+            })
+        })
+    }
+
+}
+
+register_commands(commands)
