2039c5297df75c433f06f5120872e01364f4151c
2242938055 <2242938055@qq.com>
2023-04-25 16:07:09 +0800

    /nocode add ‘createEditPage()’

---
+++ client/nocode/src/pages/base/edit/edit_form.js
@@ -0,0 +1,37 @@
+import { createFormItems } from "../form_items";
+import { edit_form_buttons } from "./edit_form_buttons";
+
+export const createEditForm = (moduleInfo) => {
+
+    const form_items = createFormItems(moduleInfo.fields)
+
+    const formEdit = {
+        com_name: 'nc_form',
+        com_ref: 'formEdit',
+        com_binds: [
+            {
+                prop_name: 'init_data',
+                field_name: 'data',
+            },
+        ],
+        com_props: {
+            form_props: {
+                "label-width": '90px',
+            },
+            commit_data: {
+                api: {
+                    url: `${moduleInfo.name}/update`,
+                    method: 'POST',
+                },
+                success_message: '修改成功',
+                error_message: '修改失败',
+            },
+            form_children: [
+                ...form_items,
+                edit_form_buttons,
+            ]
+        }
+    }
+
+    return formEdit
+}
---
+++ client/nocode/src/pages/base/edit/edit_page.js
@@ -0,0 +1,83 @@
+import { createEditForm } from "./edit_form"
+
+export const createEditPage = (moduleInfo) => {
+
+    const formEdit = createEditForm(moduleInfo)
+
+    const edit_page = {
+        com_name: "div",
+        com_version: '0',
+        com_props: {
+            style: {
+                background: '#F5F6F7',
+                flex: 1,
+                display: 'flex',
+            },
+        },
+        com_children: [
+            {
+                com_name: 'el-container',
+                com_props: {
+                    direction: 'vertical',
+                },
+                com_children: [
+                    {
+                        com_name: 'el-header',
+                        com_props: {
+                            style: {
+                                padding: '50px',
+                                'text-align': 'center',
+                            },
+                        },
+                        com_children: [
+                            {
+                                com_name: 'div',
+                                com_text: '修改产品信息',
+                            },
+                        ],
+                    },
+                    {
+                        com_name: 'el-main',
+                        com_children: [
+                            {
+                                com_name: 'div',
+                                com_props: {
+                                    style: {
+                                        width: '800px',
+                                        margin: 'auto',
+                                    },
+                                },
+                                com_children: [
+                                    {
+                                        com_name: 'nc_data',
+                                        com_props: {
+                                            fetch_data: {
+                                                api: {
+                                                    url: `${moduleInfo.name}/detail`,
+                                                    method: 'GET',
+                                                },
+                                                fetch_params: [
+                                                    {
+                                                        params_source: 'route_query',
+                                                        params_fields: [
+                                                            '_id',
+                                                        ]
+                                                    }
+                                                ]
+                                            },
+                                            data_children: [
+                                                formEdit,
+                                            ],
+                                        },
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    }
+
+    return edit_page
+}
