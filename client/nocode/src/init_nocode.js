b8dcdf7da476ee9aa7098a55319372f73d4b2d13
2242938055 <2242938055@qq.com>
2023-04-25 15:09:07 +0800

    init_nocode.js use ‘createListPage()’

--- client/nocode/src/init_nocode.js
+++ client/nocode/src/init_nocode.js
@@ -9,13 +9,15 @@ import { request_api } from './test/service'
 import { register_request_api } from './utils/nc_request'
 
 import { product_add_page } from './pages/product/add/product_add_page'
-import { product_list_page } from './pages/product/list/product_list_page'
 import { product_edit_page } from './pages/product/edit/product_edit_page'
+import { createListPage } from './pages/base/list/list_page'
+import { product_info } from './pages/product/product_info'
 
 
 export const init_nocode = () => {
     register_request_api(request_api)
     //
+    const product_list_page = createListPage(product_info)
     register_page('product/list', product_list_page)
     register_page('product/add', product_add_page)
     register_page('product/edit', product_edit_page)
---
+++ client/nocode/src/pages/product/product_info.js
@@ -0,0 +1,9 @@
+import { product_fields } from "./product_fields";
+
+export const product_info = {
+    name: 'product',
+    fields: product_fields,
+    search: {
+        placeholder: '产品名称、产品编号',
+    },
+}
