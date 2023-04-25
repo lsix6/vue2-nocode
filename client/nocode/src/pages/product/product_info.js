0c835b8b2059c5b5949c2a357bdf08263a8f25cb
2242938055 <2242938055@qq.com>
2023-04-25 15:57:14 +0800

    init_nocode.js use ‘createAddPage()’

--- client/nocode/src/pages/product/product_info.js
+++ client/nocode/src/pages/product/product_info.js
@@ -2,5 +2,11 @@ import { product_fields } from "./product_fields";
 
 export const product_info = {
     name: 'product',
+    add_form: {
+        title: '添加新产品',
+    },
+    edit_form: {
+        title: '修改产品信息',
+    },
     fields: product_fields,
 }
