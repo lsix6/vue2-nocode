8bbace019c42230b0905739fc2b9dcad042c3ea0
2242938055 <2242938055@qq.com>
2023-04-28 16:19:57 +0800

    remove ‘nc_input.vue’

--- client/coms/src/main.js
+++ client/coms/src/main.js
@@ -11,7 +11,6 @@ const registerComponents = (Vue) => {
   Vue.component('nc_pagination', () => import('./components/nc_pagination.vue'))
   Vue.component('nc_form', () => import('./components/nc_form.vue'))
   Vue.component('nc_form_item', () => import('./components/nc_form_item.vue'))
-  Vue.component('nc_input', () => import('./components/nc_input.vue'))
 }
 
 if (window.Vue) {
