c0ac49f93e0882dc5c8676ee5c7982e1041859a5
2242938055 <2242938055@qq.com>
2023-04-28 16:17:57 +0800

    init_nocode.js edit

--- client/nocode/src/init_nocode.js
+++ client/nocode/src/init_nocode.js
@@ -12,7 +12,7 @@ import { request_api } from './test/service'
 import { register_request_api, request_json } from './utils/nc_request'
 
 import { register_module } from './pages/base/pages'
-// import { product_info } from './pages/product/product_info'
+import { product_info } from './pages/product/product_info'
 
 
 export const init_nocode = async () => {
@@ -21,8 +21,9 @@ export const init_nocode = async () => {
     // console.log('***** product_info begin *****')
     // console.log(JSON.stringify(product_info))
     // console.log('***** product_info end *****')
-    await request_json('/static/modules/product.json').then(product_info => {
-        register_module(product_info)
-    })
+    // await request_json('/static/modules/product.json').then(product_info => {
+    //     register_module(product_info)
+    // })
+    register_module(product_info)
 
 }
