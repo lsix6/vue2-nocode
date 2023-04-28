83c32717e82fd60a2dd8ed646017aca11618bf33
2242938055 <2242938055@qq.com>
2023-04-28 15:49:20 +0800

    list.js edit

--- client/nocode/src/pages/base/list/list.js
+++ client/nocode/src/pages/base/list/list.js
@@ -183,7 +183,7 @@ export const createList = (moduleName, moduleFields) => {
                 com_children: [
                     {
                         com_name: 'div',
-                        com_text: '暂无数据',
+                        com_text: '暂 无 数 据',
                     },
                 ],
             }
