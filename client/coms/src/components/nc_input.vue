3e9a0a73414a06de0a53c09d17610a6ebee4498e
2242938055 <2242938055@qq.com>
2023-05-04 14:17:39 +0800

    /coms add ‘nc_input.vue’

---
+++ client/coms/src/components/nc_input.vue
@@ -0,0 +1,23 @@
+<template>
+    <el-input ref="input" v-bind="{...$attrs, ...$props}" :value="value" @input="onInput">
+    </el-input>
+</template>
+
+<script>
+export default {
+    install(Vue) {
+        Vue.component('nc_input', this)
+    },
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+    },
+    methods: {
+        onInput(v) {
+            this.$emit('input', v)
+        },
+    },
+}
+</script>
--- client/coms/src/main.js
+++ client/coms/src/main.js
@@ -11,6 +11,7 @@ const registerComponents = (Vue) => {
   Vue.component('nc_pagination', () => import('./components/nc_pagination.vue'))
   Vue.component('nc_form', () => import('./components/nc_form.vue'))
   Vue.component('nc_form_item', () => import('./components/nc_form_item.vue'))
+  Vue.component('nc_input', () => import('./components/nc_input.vue'))
 }
 
 if (window.Vue) {
