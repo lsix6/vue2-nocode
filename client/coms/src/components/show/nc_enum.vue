e5c6f1c70b23beec2c44920080f65a5dbaebb363
2242938055 <2242938055@qq.com>
2023-05-09 11:20:34 +0800

    nc_enum.vue 可以接受各种值类型 & 缺省显示 ‘-‘

--- client/coms/src/components/show/nc_enum.vue
+++ client/coms/src/components/show/nc_enum.vue
@@ -10,10 +10,7 @@ export default {
         Vue.component('nc_enum', this)
     },
     props: {
-        value: {
-            type: String,
-            default: ''
-        },
+        value: undefined,
         data_source_name: {
             type: String,
             default: ''
@@ -25,14 +22,16 @@ export default {
     },
     data() {
         return {
-            text: '',
+            text: '-',
         }
     },
     mounted() {
         // console.log('[nc_enum] mounted', this.value, typeof (this.value), this.com_params)
-        const enumMap = this.com_params[this.data_source_name]
-        if (enumMap) {
-            this.text = enumMap[this.value]
+        if (this.value !== undefined) {
+            const enumMap = this.com_params[this.data_source_name]
+            if (enumMap) {
+                this.text = enumMap['' + this.value]
+            }
         }
     },
 }
