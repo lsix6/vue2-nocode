360ab84754a68615e72edd00dae860d233308795
2242938055 <2242938055@qq.com>
2023-05-09 16:04:47 +0800

    /coms add ‘nc_color.vue’

---
+++ client/coms/src/components/show/nc_color.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-bind="{ ...$attrs, ...$props }" :style="{ background: value }">
+    </div>
+</template>
+
+<script>
+export default {
+    install(Vue) {
+        Vue.component('nc_color', this)
+    },
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+    },
+    mounted() {
+        // console.log('[nc_color] mounted', this.value, typeof (this.value))
+    },
+}
+</script>
