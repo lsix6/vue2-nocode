6385137f0e6e00179370df17d72979325b57d0ed
2242938055 <2242938055@qq.com>
2023-05-10 11:20:01 +0800

    nc_enum.vue add prop ‘enum_map’

--- client/coms/src/components/show/nc_enum.vue
+++ client/coms/src/components/show/nc_enum.vue
@@ -15,6 +15,10 @@ export default {
             type: String,
             default: ''
         },
+        enum_map: {
+            type: Object,
+            default: null
+        },
         com_params: {
             type: Object,
             default: null
@@ -28,9 +32,13 @@ export default {
     mounted() {
         // console.log('[nc_enum] mounted', this.value, typeof (this.value), this.com_params)
         if (this.value !== undefined) {
-            const enumMap = this.com_params[this.data_source_name]
-            if (enumMap) {
-                this.text = enumMap['' + this.value]
+            if (this.data_source_name) {
+                const enumMap = this.com_params[this.data_source_name]
+                if (enumMap) {
+                    this.text = enumMap['' + this.value]
+                }
+            } else if (this.enum_map) {
+                this.text = this.enum_map['' + this.value]
             }
         }
     },
