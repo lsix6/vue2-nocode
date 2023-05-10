061a1666306e1996b5bb0e6fbcfa3ca38996affd
2242938055 <2242938055@qq.com>
2023-05-10 11:31:03 +0800

    service.js edit

--- client/nocode/src/test/service.js
+++ client/nocode/src/test/service.js
@@ -9,12 +9,15 @@ const init_products = () => {
             _id: '' + i,
             no: '' + i,
             name: '' + i,
+            picture: `/static/imgs/img${i % 5}.png`,
             price: i * 10,
-            market: '0',
-            producePlace: '1',
+            market: (Math.random() > 0.5 ? '0' : '1'),
+            producePlace: (Math.floor(Math.random() * 5)).toString(),
+            inSale: (Math.random() > 0.5),
+            delivery: (Math.random() > 0.5),
             storageDate: '2023-05-01 00:00:00',
             color: '#0000ff',
-            picture: `/static/imgs/img${i % 5}.png`
+            level: (Math.floor(Math.random() * 6)),
         })
     }
 }
