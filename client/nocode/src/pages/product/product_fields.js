1d9ddfb8ea2f422843853faeec7e15ca6ec910b3
2242938055 <2242938055@qq.com>
2023-05-11 10:53:35 +0800

    product_fields.js add search ‘level

--- client/nocode/src/pages/product/product_fields.js
+++ client/nocode/src/pages/product/product_fields.js
@@ -545,6 +545,54 @@ export const product_fields = [
                 }
             ],
         },
+        in_search: {
+            search_com: {
+                com_name: 'div',
+                com_info: {
+                    style: {
+                        display: 'flex',
+                        'flex-direction': 'row',
+                        'margin-right': '20px',
+                    },
+                },
+                com_children: [
+                    {
+                        com_name: 'div',
+                        com_text: '等级',
+                        com_info: {
+                            style: {
+                                'line-height': '40px',
+                                'white-space': 'nowrap',
+                                'margin-right': '10px',
+                            },
+                        },
+                    },
+                    {
+                        com_name: 'nc_select',
+                        com_info: {
+                            style: {
+                                width: '90px',
+                            },
+                        },
+                        com_field: {
+                            field_name: 'formData.level',
+                        },
+                        com_props: {
+                            value_type: 'Number',
+                            enum_map: {
+                                '': '全部',
+                                '0': '0',
+                                '1': '1',
+                                '2': '2',
+                                '3': '3',
+                                '4': '4',
+                                '5': '5',
+                            },
+                        },
+                    },
+                ],
+            },
+        },
         in_form: {
             edit_com: {
                 com_name: 'el-rate',
