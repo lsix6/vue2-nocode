5ac65150321fa3ff81699d23d567bac371c6f6cb
2242938055 <2242938055@qq.com>
2023-05-11 14:55:11 +0800

    /coms add ‘nc_enum_color.vue’

---
+++ client/coms/src/components/show/nc_enum_color.vue
@@ -0,0 +1,56 @@
+<template>
+    <div v-bind="{ ...$attrs, ...$props }" :style="{ background: color }">
+    </div>
+</template>
+
+<script>
+export default {
+    install(Vue) {
+        Vue.component('nc_enum_color', this)
+    },
+    props: {
+        value: undefined,
+        data_source_name: {
+            type: String,
+            default: ''
+        },
+        enum_map: {
+            type: Object,
+            default: null
+        },
+        com_params: {
+            type: Object,
+            default: null
+        },
+    },
+    data() {
+        return {
+            color: 'transparent',
+        }
+    },
+    mounted() {
+        // console.log('[nc_enum_color] mounted', this.value, typeof (this.value))
+        //
+        this.updateColor()
+    },
+    beforeUpdate() {
+        // console.log('[nc_enum_color] beforeUpdate', this.value, typeof (this.value))
+        //
+        this.updateColor()
+    },
+    methods: {
+        updateColor() {
+            if (this.value !== undefined) {
+                if (this.data_source_name) {
+                    const enumMap = this.com_params[this.data_source_name]
+                    if (enumMap) {
+                        this.color = enumMap['' + this.value]
+                    }
+                } else if (this.enum_map) {
+                    this.color = this.enum_map['' + this.value]
+                }
+            }
+        },
+    },
+}
+</script>
