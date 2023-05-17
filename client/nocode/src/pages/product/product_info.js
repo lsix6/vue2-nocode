3332d4571e916eb1ef38bb2d53190fd3557d272a
2242938055 <2242938055@qq.com>
2023-05-17 14:06:37 +0800

    product_fields.js edit field ‘producePlace’

--- client/nocode/src/pages/product/product_info.js
+++ client/nocode/src/pages/product/product_info.js
@@ -4,14 +4,6 @@ const moduleName = 'product'
 
 export const product_info = {
     name: moduleName,
-    data_sources: {
-        __producePlaceMap: {
-            api: {
-                url: `${moduleName}/producePlace`,
-                method: 'GET',
-            },
-        },
-    },
     add_form: {
         title: '添加新产品',
     },
