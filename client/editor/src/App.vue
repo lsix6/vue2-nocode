0d73cebbed4a6f6e67b03547f3d0aab6e5ce88f9
2242938055 <2242938055@qq.com>
2023-05-18 16:55:03 +0800

    /editor App.vue edit css

--- client/editor/src/App.vue
+++ client/editor/src/App.vue
@@ -17,11 +17,10 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #fff;
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+  padding: 10px;
 }
 </style>
--- client/editor/src/components/Editor.vue
+++ client/editor/src/components/Editor.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="nocode-editor">
 
     </div>
 </template>
@@ -8,3 +8,10 @@
 export default {
 }
 </script>
+
+<style lang="scss">
+.nocode-editor {
+    flex: 1;
+    background-color: aliceblue;
+}
+</style>
