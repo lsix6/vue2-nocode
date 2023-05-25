bc859a7136cf9d565f1f6120c11129c7e07ba31a
2242938055 <2242938055@qq.com>
2023-05-25 16:03:21 +0800

    Editor.vue use ‘grapesjs’

--- client/editor/src/components/Editor.vue
+++ client/editor/src/components/Editor.vue
@@ -1,136 +1,50 @@
 <template>
-    <el-container class="nocode-editor">
-        <el-aside class="left">
-            <el-tree class="node-tree">
-
-            </el-tree>
-        </el-aside>
-        <el-main class="right">
-            <div class="content">
-                <nc_component v-bind="nodes.root"></nc_component>
-            </div>
-        </el-main>
-        <ComsListDlg ref="dlgComList" @select="addCom"></ComsListDlg>
-    </el-container>
+    <div class="content" id="gjs">
+        <h1>Hello World Component!</h1>
+    </div>
 </template>
 
 <script>
 
-import ComsListDlg from './ComsListDlg.vue'
+import 'grapesjs/dist/css/grapes.min.css'
+import grapesjs from 'grapesjs'
 
 export default {
-
-    components: {
-        ComsListDlg,
-    },
-
     data() {
         return {
-            nodes: {
-                root: null,
-                lastId: 1,
-            },
         }
     },
     mounted() {
-        window.nocode.register_com_ref('dlgComList', this.$refs.dlgComList)
-        this.nodes.root = this.createContainerNode()
+        this.initGrapesjs()
     },
     methods: {
-        onAdd(e) {
-            console.log('onAdd', e)
-            //
-            this.$refs.dlgComList.show()
-        },
-        addCom(comId) {
-            console.log('addCom', comId)
-        },
-        generateNewNode() {
-            const node = {
-                id: this.nodes.lastId,
-            }
-            //
-            this.nodes.lastId++
-            //
-            return node
-        },
-        createContainerNode() {
-            const base = this.generateNewNode()
-            //
-            const node = {
-                ...base,
-                com_name: 'div',
-                com_info: {
-                    style: {
-                        flex: 1,
-                        display: 'flex',
-                        'flex-direction': 'row',
-                    },
+        initGrapesjs() {
+            grapesjs.init({
+                container: '#gjs',
+                fromElement: true,
+                height: 'auto',
+                width: 'auto',
+                storageManager: false,
+                panels: {
+                    defaults: []
                 },
-                com_children: [
-                    {
-                        com_name: 'el-button',
-                        com_info: {
-                            class: 'btn-add',
-                        },
-                        com_children: [
-                            {
-                                com_name: 'i',
-                                com_info: {
-                                    class: 'el-icon-plus',
-                                },
-                            },
-                        ],
-                        com_events: [
-                            {
-                                event_name: 'click',
-                                commands: [
-                                    {
-                                        cmd_name: 'call_com_method',
-                                        cmd_params: {
-                                            com_ref: 'dlgComList',
-                                            com_method_name: 'show',
-                                        },
-                                    }
-                                ]
-                            }
-                        ],
-                    },
-                ],
-            }
-            //
-            return node
+            })
         },
     },
 }
 </script>
 
 <style lang="scss">
-.nocode-editor {
-    background-color: aliceblue;
+/* Let's highlight canvas boundaries */
+#gjs {
+    /* border: 3px solid #444; */
+    flex: 1;
+}
 
-    .left {
-        padding: 10px;
-        display: flex;
-
-        .node-tree {
-            flex: 1;
-        }
-    }
-
-    .right {
-        display: flex;
-        padding: 10px;
-
-        .content {
-            flex: 1;
-            display: flex;
-            background-color: white;
-
-            .btn-add {
-                margin: auto;
-            }
-        }
-    }
+/* Reset some default styling */
+.gjs-cv-canvas {
+    top: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
