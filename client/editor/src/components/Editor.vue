c56a0e7673ba4e72f403030ed5935ab28267cb9e
2242938055 <2242938055@qq.com>
2023-05-25 17:22:16 +0800

    Editor.vue add panels & blocks

--- client/editor/src/components/Editor.vue
+++ client/editor/src/components/Editor.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="content" id="gjs">
-        <h1>Hello World Component!</h1>
+    <div class="editor-frame">
+        <div class="panel__top">
+            <div class="panel__basic-actions"></div>
+        </div>
+        <div id="gjs">
+            <h1>Hello World Component!</h1>
+        </div>
+        <div id="blocks"></div>
     </div>
 </template>
 
@@ -19,26 +25,137 @@ export default {
     },
     methods: {
         initGrapesjs() {
-            grapesjs.init({
+            const initCustomComTypes = editor => {
+                editor.DomComponents.addType('my-type', {
+                    isComponent: el => el.tagName === 'MYCOM',
+                    model: {
+                        defaults: {
+                            tagName: 'div',
+                        },
+                    },
+                    view: {
+                        tagName: 'div',
+                        onRender({ el }) {
+                            const btn = document.createElement('button')
+                            btn.innerText = 'XXX'
+                            el.appendChild(btn)
+                        },
+                    },
+                })
+            }
+
+            const editor = grapesjs.init({
                 container: '#gjs',
                 fromElement: true,
-                height: 'auto',
+                height: '300px',
                 width: 'auto',
                 storageManager: false,
                 panels: {
                     defaults: []
                 },
+                plugins: [
+                    initCustomComTypes
+                ],
+                blockManager: {
+                    appendTo: '#blocks',
+                    blocks: [
+                        {
+                            id: 'section', // id is mandatory
+                            label: '<b>Section</b>', // You can use HTML/SVG inside labels
+                            attributes: { class: 'gjs-block-section' },
+                            content: `<mycom>AAA</mycom>`,
+                        }, {
+                            id: 'text',
+                            label: 'Text',
+                            content: '<div data-gjs-type="text">Insert your text here</div>',
+                        }, {
+                            id: 'image',
+                            label: 'Image',
+                            // Select the component once it's dropped
+                            select: true,
+                            // You can pass components as a JSON instead of a simple HTML string,
+                            // in this case we also use a defined component type `image`
+                            content: { type: 'image' },
+                            // This triggers `active` event on dropped components and the `image`
+                            // reacts by opening the AssetManager
+                            activate: true,
+                        }
+                    ]
+                },
             })
+
+            editor.Panels.addPanel({
+                id: 'panel-top',
+                el: '.panel__top',
+            });
+            editor.Panels.addPanel({
+                id: 'basic-actions',
+                el: '.panel__basic-actions',
+                buttons: [
+                    {
+                        id: 'visibility',
+                        active: true, // active by default
+                        className: 'btn-toggle-borders',
+                        label: '<u>B</u>',
+                        command: 'sw-visibility', // Built-in command
+                    },
+                    {
+                        id: 'export',
+                        className: 'btn-open-export',
+                        label: 'Exp',
+                        command: 'export-template',
+                        context: 'export-template', // For grouping context of buttons from the same panel
+                    },
+                    {
+                        id: 'show-json',
+                        className: 'btn-show-json',
+                        label: 'JSON',
+                        context: 'show-json',
+                        command(editor) {
+                            editor.Modal.setTitle('Components JSON')
+                                .setContent(`<textarea style="width:100%; height: 250px;">
+            ${JSON.stringify(editor.getComponents())}
+          </textarea>`)
+                                .open();
+                        },
+                    },
+                    {
+                        id: 'preview',
+                        className: 'btn-show-json',
+                        label: 'PREVIEW',
+                        context: 'preview',
+                        command(editor) {
+                            editor.runCommand('preview')
+                        },
+                    },
+                ],
+            });
         },
     },
 }
 </script>
 
 <style lang="scss">
+.editor-frame {
+    flex: 1;
+}
+
+.panel__top {
+    padding: 0;
+    width: 100%;
+    display: flex;
+    position: initial;
+    justify-content: center;
+    justify-content: space-between;
+}
+
+.panel__basic-actions {
+    position: initial;
+}
+
 /* Let's highlight canvas boundaries */
 #gjs {
-    /* border: 3px solid #444; */
-    flex: 1;
+    border: 3px solid #444;
 }
 
 /* Reset some default styling */
@@ -46,5 +163,11 @@ export default {
     top: 0;
     width: 100%;
     height: 100%;
+}
+
+.gjs-block {
+    width: auto;
+    height: auto;
+    min-height: auto;
 }
 </style>
