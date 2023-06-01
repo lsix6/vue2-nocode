aeb5a63f9a81350278d76a0e94e0228bf8ffacc9
2242938055 <2242938055@qq.com>
2023-06-01 14:28:52 +0800

    NestedEditor.vue format

--- client/editor/src/views/editor/components/NestedEditor.vue
+++ client/editor/src/views/editor/components/NestedEditor.vue
@@ -1,32 +1,16 @@
 <template>
-    <draggable
-        ref="draggable"
-        :list="childComponentList"
-        v-bind="dragOptions"
-        :class="[$style.dragArea, $style.formItemWrap]"
-        @change="handleDragChange"
-    >
-        <div
-            v-for="item in childComponentList"
-            :key="item.id"
-            :class="{
-                draggableItem: true,
-                w100: showNestedEditor(item),
-                [$style.formItem]: true
-            }"
-            :style=" item.componentValue.baseValue.uiOptions.width ? {
-                width: item.componentValue.baseValue.uiOptions.width,
-                flexBasis: item.componentValue.baseValue.uiOptions.width
-            } : {}"
-        >
-            <ViewComponentWrap
-                :form-data="formData"
-                :editor-item="item"
-                :drag-options="dragOptions"
-                :show-nested-editor="showNestedEditor"
-                :form-props="formProps"
-                @onOperate="handleItemOperate"
-            >
+    <draggable ref="draggable" :list="childComponentList" v-bind="dragOptions"
+        :class="[$style.dragArea, $style.formItemWrap]" @change="handleDragChange">
+        <div v-for="item in childComponentList" :key="item.id" :class="{
+            draggableItem: true,
+            w100: showNestedEditor(item),
+            [$style.formItem]: true
+        }" :style="item.componentValue.baseValue.uiOptions.width ? {
+    width: item.componentValue.baseValue.uiOptions.width,
+    flexBasis: item.componentValue.baseValue.uiOptions.width
+} : {}">
+            <ViewComponentWrap :form-data="formData" :editor-item="item" :drag-options="dragOptions"
+                :show-nested-editor="showNestedEditor" :form-props="formProps" @onOperate="handleItemOperate">
             </ViewComponentWrap>
         </div>
         <template slot="footer">
@@ -126,140 +110,161 @@ export default {
 </script>
 
 <style module>
-    @import "../../../assets/css/variable.css";
-    :global {
-        .ghostItem {
-            opacity: 0.6;
-            background-color: color(var(--color-primary) a(0.4)) !important;
-            box-shadow: 0 0 1px 0 var(--color-primary);
-            &.draggableToolItem {
-                margin-top: 0;
-                width: 100%;
-                max-width: 100%;
-                margin-bottom: 10px;
-                height: 130px;
-                line-height: 130px;
-            }
+@import "../../../assets/css/variable.css";
+
+:global {
+    .ghostItem {
+        opacity: 0.6;
+        background-color: color(var(--color-primary) a(0.4)) !important;
+        box-shadow: 0 0 1px 0 var(--color-primary);
+
+        &.draggableToolItem {
+            margin-top: 0;
+            width: 100%;
+            max-width: 100%;
+            margin-bottom: 10px;
+            height: 130px;
+            line-height: 130px;
         }
-        .el-form--label-left,.el-form--label-right {
-            .draggableToolItem {
-                height: 100px;
-                line-height: 100px;
-            }
+    }
+
+    .el-form--label-left,
+    .el-form--label-right {
+        .draggableToolItem {
+            height: 100px;
+            line-height: 100px;
+        }
+    }
+
+    /* inline 布局 */
+    .el-form--inline {
+        :local .formItem {
+            display: inline-block;
+            margin-right: 10px;
+            vertical-align: top;
         }
 
-        /* inline 布局 */
-        .el-form--inline {
-            :local .formItem {
-                display: inline-block;
-                margin-right: 10px;
-                vertical-align: top;
-            }
-            .draggableToolItem{
-                width: auto;
-                max-width: none;
-                padding: 0 20px;
-                display: inline-block;
-                margin-right: 10px;
-                vertical-align: top;
-            }
-            .w100 {
-                width: 100%;
-            }
-            .formFooter_item-editor {
-                width: auto;
-                max-width: none;
-            }
+        .draggableToolItem {
+            width: auto;
+            max-width: none;
+            padding: 0 20px;
+            display: inline-block;
+            margin-right: 10px;
+            vertical-align: top;
         }
 
-        /* column 布局 */
-        .layoutColumn {
-            .w100 {
-                width: 100% !important;
-            }
-            :local {
-                .formItemWrap {
-                    :global {
-                        .el-form-item {
-                            width: 100% !important;
-                            flex-basis: 100% !important;
-                        }
+        .w100 {
+            width: 100%;
+        }
+
+        .formFooter_item-editor {
+            width: auto;
+            max-width: none;
+        }
+    }
+
+    /* column 布局 */
+    .layoutColumn {
+        .w100 {
+            width: 100% !important;
+        }
+
+        :local {
+            .formItemWrap {
+                :global {
+                    .el-form-item {
+                        width: 100% !important;
+                        flex-basis: 100% !important;
                     }
                 }
             }
+        }
+
+        :local .formItem {
+            display: inline-block;
+            vertical-align: top;
+        }
+
+        .draggableToolItem {
+            display: inline-block;
+            vertical-align: top;
+            border: none;
+        }
+
+        &.layoutColumn-1 {
             :local .formItem {
-                display: inline-block;
-                vertical-align: top;
+                width: 100%;
             }
+
             .draggableToolItem {
-                display: inline-block;
-                vertical-align: top;
-                border: none;
+                width: 100%;
             }
-            &.layoutColumn-1 {
-                :local .formItem{
-                    width: 100%;
-                }
-                .draggableToolItem{
-                    width: 100%;
-                }
+        }
+
+        &.layoutColumn-2 {
+            :local .formItem {
+                width: 50%;
             }
-            &.layoutColumn-2 {
-                :local .formItem{
-                    width: 50%;
-                }
-                .draggableToolItem{
-                    width: 49.6%;
-                }
+
+            .draggableToolItem {
+                width: 49.6%;
             }
-            &.layoutColumn-3 {
-                :local .formItem {
-                    width: 33.333%;
-                }
-                .draggableToolItem {
-                    width: 33%;
-                }
+        }
+
+        &.layoutColumn-3 {
+            :local .formItem {
+                width: 33.333%;
+            }
+
+            .draggableToolItem {
+                width: 33%;
+            }
+        }
+    }
+}
+
+.dragArea {
+    width: 100%;
+    box-sizing: border-box;
+    font-size: 0;
+    background-color: #f5f5f5;
+    height: 100%;
+    padding: 15px;
+    overflow: auto;
+
+    .dragArea {
+        &:empty {
+            padding: 20px;
+
+            &:after {
+                display: block;
+                text-align: center;
+                font-size: 12px;
+                content: '拖入子组件';
             }
         }
     }
 
-    .dragArea {
-        width: 100%;
-        box-sizing: border-box;
-        font-size: 0;
-        background-color: #f5f5f5;
-        height: 100%;
-        padding: 15px;
-        overflow: auto;
-        .dragArea {
-            &:empty {
-                padding: 20px;
-                &:after {
-                    display: block;
-                    text-align: center;
-                    font-size: 12px;
-                    content: '拖入子组件';
-                }
-            }
+    :global {
+        .fieldGroupWrap_box {
+            display: none !important;
         }
-        :global {
-            .fieldGroupWrap_box {
-                display: none !important;
-            }
-            .emptyBox {
-                min-height: 350px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-            .viewEmpty_IconBox {
-                color: color(var(--checkbox-color) a(0.7));
-                font-size: 50px;
-                text-align: center;
-            }
-            .el-image {
-                vertical-align: top;
-            }
+
+        .emptyBox {
+            min-height: 350px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .viewEmpty_IconBox {
+            color: color(var(--checkbox-color) a(0.7));
+            font-size: 50px;
+            text-align: center;
+        }
+
+        .el-image {
+            vertical-align: top;
         }
     }
-</style>
+}</style>
