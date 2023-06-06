b0358f09d94ce1ffa0e79c85b329fc91aef0b9c9
2242938055 <2242938055@qq.com>
2023-06-06 17:33:54 +0800

    /coms add ‘e_nc_enum.vue’

---
+++ client/coms/src/e_components/show/e_nc_enum.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="font-size: 14px;">
+        enum
+    </div>
+</template>
+
+<script>
+export default {
+    install(Vue) {
+        Vue.component('e_nc_enum', this)
+    },
+    props: {
+        editorItem: {
+            type: Object,
+            default: null,
+        },
+    },
+    mounted() {
+        // console.log('[e_nc_enum] mounted()', this)
+    },
+    methods: {
+        getComValue() {
+            const options = this.editorItem.componentValue.options
+            const obj = {
+                com_name: "nc_enum",
+                com_props: {
+                    data_source_name: options.uiOptions.data_source_name
+                }
+            }
+            //
+            return obj
+        },
+    },
+}
+</script>
