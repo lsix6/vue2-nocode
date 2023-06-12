1abc7d98c37c9044bfbe8458360c7634b3f659c2
2242938055 <2242938055@qq.com>
2023-06-12 10:57:20 +0800

    /editor add ‘ComsEditor.vue’

--- client/editor/src/App.vue
+++ client/editor/src/App.vue
@@ -1,89 +1,22 @@
 <template>
   <div id="app">
-    <el-tabs class="com-editor-tabs" v-model="editableTabsValue" type="card" editable @edit="handleTabsEdit">
-      <el-tab-pane class="tabs-content" :key="item.name" v-for="(item) in editableTabs" :label="item.title"
-        :name="item.name">
-        {{ item.content }}
-        <Editor />
-      </el-tab-pane>
-    </el-tabs>
+    <ComsEditor />
   </div>
 </template>
 
 <script>
-import Editor from './views/editor/Editor.vue'
+import ComsEditor from './views/editor/ComsEditor.vue'
 
 export default {
   name: 'App',
   components: {
-    Editor
-  },
-  data() {
-    return {
-      editableTabsValue: '2',
-      editableTabs: [{
-        title: 'Tab 1',
-        name: '1',
-        content: 'Tab 1 content'
-      }, {
-        title: 'Tab 2',
-        name: '2',
-        content: 'Tab 2 content'
-      }],
-      tabIndex: 2
-    }
-  },
-  methods: {
-    handleTabsEdit(targetName, action) {
-      if (action === 'add') {
-        let newTabName = ++this.tabIndex + '';
-        this.editableTabs.push({
-          title: 'New Tab',
-          name: newTabName,
-          content: 'New Tab content'
-        });
-        this.editableTabsValue = newTabName;
-      }
-      if (action === 'remove') {
-        let tabs = this.editableTabs;
-        let activeName = this.editableTabsValue;
-        if (activeName === targetName) {
-          tabs.forEach((tab, index) => {
-            if (tab.name === targetName) {
-              let nextTab = tabs[index + 1] || tabs[index - 1];
-              if (nextTab) {
-                activeName = nextTab.name;
-              }
-            }
-          });
-        }
-
-        this.editableTabsValue = activeName;
-        this.editableTabs = tabs.filter(tab => tab.name !== targetName);
-      }
-    }
+    ComsEditor
   }
 }
 </script>
 
-<style lang="scss">
+<style>
 #app {
   height: 100%;
-}
-
-.com-editor-tabs {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-
-  .el-tabs__content {
-    flex: 1;
-    display: flex;
-
-    .tabs-content {
-      flex: 1;
-    }
-  }
-
 }
 </style>
