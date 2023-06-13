5339497dd7f72f12b6b9045db818176944b17266
2242938055 <2242938055@qq.com>
2023-06-13 17:12:21 +0800

    nc_com.vue 把 ‘slotProps.com_data’ 传到子节点中

--- client/nocode/src/components/core/nc_com.vue
+++ client/nocode/src/components/core/nc_com.vue
@@ -4,7 +4,7 @@
             <slot v-bind="slotProps"></slot>
             <template v-if="com_children && com_children.length > 0">
                 <nc_com ref="item" v-for="( child, index ) in  com_children " :key="'child_' + index" v-bind="child"
-                    :com_data="{ ...com_data, ...slotProps }">
+                    :com_data="{ ...com_data, ...slotProps.com_data }">
                 </nc_com>
             </template>
         </template>
@@ -28,7 +28,7 @@ export default {
         },
     },
     mounted() {
-        // console.log('[nc_com] mounted()', this)
+        // console.log('[nc_com] mounted()', this.$attrs.com_name, this)
     },
 }
 </script>
