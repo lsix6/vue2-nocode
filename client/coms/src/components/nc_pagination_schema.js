0f453256b72433c4e19e1df333256db3467c4069
2242938055 <2242938055@qq.com>
2023-06-27 15:28:10 +0800

    nc_pagination_schema.js add ‘com_events’

--- client/coms/src/components/nc_pagination_schema.js
+++ client/coms/src/components/nc_pagination_schema.js
@@ -15,7 +15,66 @@ export default {
             title: 'props',
             properties: {
             }
-        }
+        },
+        com_events: {
+            type: 'array',
+            title: 'events',
+            minItems: 0,
+            'ui:showIndexNumber': true,
+            items: {
+                type: 'object',
+                title: 'event',
+                properties: {
+                    event_name: {
+                        type: 'string',
+                        title: 'name',
+                    },
+                    commands: {
+                        type: 'array',
+                        title: 'commands',
+                        minItems: 0,
+                        'ui:showIndexNumber': true,
+                        items: {
+                            type: 'object',
+                            title: 'command',
+                            properties: {
+                                cmd_name: {
+                                    type: 'string',
+                                    title: 'name',
+                                },
+                                cmd_params: {
+                                    type: 'object',
+                                    title: 'params',
+                                    properties: {
+                                        com_ref: {
+                                            type: 'string',
+                                            title: 'ref',
+                                        },
+                                        com_method_name: {
+                                            type: 'string',
+                                            title: 'method name',
+                                        },
+                                        com_method_params: {
+                                            type: 'array',
+                                            title: 'method params',
+                                            items: {
+                                                type: 'object',
+                                                properties: {
+                                                    ds_name: {
+                                                        type: 'string',
+                                                        title: 'ds name',
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+        },
     },
     comSchema: {
         com_name: 'nc_pagination',
