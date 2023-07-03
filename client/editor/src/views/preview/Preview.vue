8864767a69fa23bd3fb738e4837737184f82272d
2242938055 <2242938055@qq.com>
2023-07-03 11:10:25 +0800

    Preview.vue edit

--- client/editor/src/views/preview/Preview.vue
+++ client/editor/src/views/preview/Preview.vue
@@ -1,5 +1,40 @@
 <template>
-    <div>
-        Preview
+    <div id="app">
+        <el-aside width="160px" style="display: flex; border: 1px solid #eee; background-color: rgb(238, 241, 246)">
+            <el-menu :router="true" style="flex: 1; display: flex; flex-direction: column;">
+                <el-menu-item index="/product/list" style="margin: 10px; text-align: center; background-color: aliceblue;">
+                    产品列表
+                </el-menu-item>
+                <el-menu-item index="/user/list" style="margin: 10px; text-align: center; background-color: aliceblue;">
+                    用户列表
+                </el-menu-item>
+            </el-menu>
+        </el-aside>
+        <el-container>
+            <el-header height="48px" style="border: 1px solid #eee; text-align: right; font-size: 12px">
+
+            </el-header>
+            <el-main style="display: flex;">
+                <nc_view />
+            </el-main>
+        </el-container>
     </div>
 </template>
+  
+<script>
+
+export default {
+    name: 'Preview',
+}
+</script>
+  
+<style>
+#Preview {
+    background-color: #fff;
+    flex: 1;
+    display: flex;
+    overflow: hidden;
+
+}
+</style>
+  
