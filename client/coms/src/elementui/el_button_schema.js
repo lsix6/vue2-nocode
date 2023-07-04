74e85e72124edf8b5ef0a7328f7be1e50baa86c3
2242938055 <2242938055@qq.com>
2023-07-04 14:34:47 +0800

    el_button_schema.js add ‘com_events’

--- client/coms/src/elementui/el_button_schema.js
+++ client/coms/src/elementui/el_button_schema.js
@@ -33,7 +33,69 @@ export default {
                     title: 'type',
                 },
             }
-        }
+        },
+        com_events: {
+            type: 'array',
+            title: 'events',
+            minItems: 0,
+            'ui:showIndexNumber': true,
+            items: {
+                type: 'object',
+                title: 'event',
+                properties: {
+                    event_name: {
+                        type: 'string',
+                        title: 'name',
+                    },
+                    commands: {
+                        type: 'array',
+                        title: 'commands',
+                        minItems: 0,
+                        'ui:showIndexNumber': true,
+                        items: {
+                            type: 'object',
+                            title: 'command',
+                            properties: {
+                                cmd_name: {
+                                    type: 'string',
+                                    title: 'name',
+                                },
+                                cmd_params: {
+                                    type: 'object',
+                                    title: 'params',
+                                    properties: {
+                                        path: {
+                                            type: 'string',
+                                            title: 'path',
+                                        },
+                                        route_params: {
+                                            type: 'array',
+                                            title: 'route params',
+                                            items: {
+                                                type: 'object',
+                                                properties: {
+                                                    params_source: {
+                                                        type: 'string',
+                                                        title: 'params source',
+                                                    },
+                                                    params_fields: {
+                                                        type: 'array',
+                                                        title: 'params fields',
+                                                        items: {
+                                                            type: 'string'
+                                                        },
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+        },
     },
     comSchema: {
         com_name: 'el-button',
