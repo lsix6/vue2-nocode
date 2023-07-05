6f5bd5d948ea7f9de35870ce2f7b2d3c115b9927
2242938055 <2242938055@qq.com>
2023-07-05 14:10:38 +0800

    NestedEditor.vue edit css

--- client/editor/src/views/editor/components/NestedEditor.vue
+++ client/editor/src/views/editor/components/NestedEditor.vue
@@ -8,7 +8,7 @@
         }" :style="item.componentValue.baseValue.uiOptions.width ? {
     width: item.componentValue.baseValue.uiOptions.width,
     flexBasis: item.componentValue.baseValue.uiOptions.width
-} : {}" style="flex: 1;">
+} : {}">
             <ViewComponentWrap :form-data="formData" :editor-item="item" :drag-options="dragOptions"
                 :show-nested-editor="showNestedEditor" :form-props="formProps" @onOperate="handleItemOperate"
                 :com_root="com_root" :com_data="com_data">
