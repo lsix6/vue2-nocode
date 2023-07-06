7a87ec1029a83034f37899a52a98eefbdf4fe389
2242938055 <2242938055@qq.com>
2023-07-06 13:45:58 +0800

    el_popconfirm_schema.js add ‘com_events’

--- client/coms/src/elementui/el_popconfirm_schema.js
+++ client/coms/src/elementui/el_popconfirm_schema.js
@@ -28,7 +28,143 @@ export default {
                     default: '删除后不可恢复，确认删除吗？',
                 },
             }
-        }
+        },
+        com_events: {
+            type: 'array',
+            title: 'events',
+            minItems: 0,
+            'ui:showIndexNumber': true,
+            items: {
+                type: 'object',
+                title: 'event',
+                properties: {
+                    event_name: {
+                        type: 'string',
+                        title: 'name',
+                    },
+                    commands: {
+                        type: 'array',
+                        title: 'commands',
+                        minItems: 0,
+                        'ui:showIndexNumber': true,
+                        items: {
+                            type: 'object',
+                            title: 'command',
+                            properties: {
+                                cmd_name: {
+                                    type: 'string',
+                                    title: 'name',
+                                },
+                                cmd_params: {
+                                    type: 'object',
+                                    title: 'params',
+                                    properties: {
+                                        api: {
+                                            type: 'object',
+                                            title: 'api',
+                                            properties: {
+                                                url: {
+                                                    title: 'url',
+                                                    type: 'string'
+                                                },
+                                                method: {
+                                                    type: 'string',
+                                                    title: 'method',
+                                                    enum: [
+                                                        'GET',
+                                                    ],
+                                                    default: 'GET'
+                                                },
+                                            },
+                                        },
+                                        request_params: {
+                                            type: 'array',
+                                            title: 'params',
+                                            items: {
+                                                type: 'object',
+                                                title: 'param',
+                                                properties: {
+                                                    params_source: {
+                                                        type: 'string',
+                                                        title: 'source',
+                                                    },
+                                                    params_fields: {
+                                                        type: 'array',
+                                                        title: 'fields',
+                                                        items: {
+                                                            type: 'string',
+                                                        },
+                                                    },
+                                                    params_com_ref: {
+                                                        type: 'string',
+                                                        title: 'com ref',
+                                                    },
+                                                    params_com_method_name: {
+                                                        type: 'string',
+                                                        title: 'method name',
+                                                    },
+                                                    params_default_value: {
+                                                        type: 'string',
+                                                        title: 'default value',
+                                                    },
+                                                    params_set_name: {
+                                                        type: 'string',
+                                                        title: 'set name',
+                                                    },
+                                                },
+                                            },
+                                        },
+                                        commands: {
+                                            type: 'array',
+                                            title: 'commands',
+                                            minItems: 0,
+                                            'ui:showIndexNumber': true,
+                                            items: {
+                                                type: 'object',
+                                                title: 'command',
+                                                properties: {
+                                                    cmd_name: {
+                                                        type: 'string',
+                                                        title: 'name',
+                                                    },
+                                                    cmd_params: {
+                                                        type: 'object',
+                                                        title: 'params',
+                                                        properties: {
+                                                            com_ref: {
+                                                                type: 'string',
+                                                                title: 'ref',
+                                                            },
+                                                            com_method_name: {
+                                                                type: 'string',
+                                                                title: 'method name',
+                                                            },
+                                                            com_method_params: {
+                                                                type: 'array',
+                                                                title: 'method params',
+                                                                items: {
+                                                                    type: 'object',
+                                                                    properties: {
+                                                                        ds_name: {
+                                                                            type: 'string',
+                                                                            title: 'ds name',
+                                                                        },
+                                                                    },
+                                                                },
+                                                            },
+                                                        },
+                                                    },
+                                                },
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            }
+        },
     },
     comSchema: {
         com_name: 'ElPopconfirm',
