add37c0d2cbbd73467f5a75cc2d618fc9d8c0003
2242938055 <2242938055@qq.com>
2023-07-14 15:14:55 +0800

    /coms add ‘nc_module_view.vue’

--- client/coms/src/components/module/nc_module.vue
+++ client/coms/src/components/module/nc_module.vue
@@ -1,16 +1,26 @@
 <template>
-    <div>
+    <div v-if="isInEdit">
         <div>{{ module_label }}({{ module_name }})</div>
         <slot></slot>
     </div>
+    <nc_module_view v-else :module_label="module_label" />
 </template>
 
 <script>
+import nc_module_view from './nc_module_view.vue'
+
 export default {
     install(Vue) {
         Vue.component('nc_module', this)
     },
+    components: {
+        nc_module_view,
+    },
     props: {
+        com_root: {
+            type: Object,
+            default: null
+        },
         module_name: {
             type: String,
             default: '',
@@ -19,6 +29,14 @@ export default {
             type: String,
             default: '',
         },
+    },
+    computed: {
+        isInEdit() {
+            return this.com_root.isInEdit
+        },
+    },
+    mounted() {
+        console.log('[nc_module] com_root: ', this.com_root)
     },
 }
 </script>
---
+++ client/coms/src/components/module/nc_module_view.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>{{ module_label }}列表</div>
+    </div>
+</template>
+
+<script>
+export default {
+    install(Vue) {
+        Vue.component('nc_module_view', this)
+    },
+    props: {
+        module_label: {
+            type: String,
+            default: '',
+        },
+    },
+    mounted() {
+    },
+}
+</script>
