8226650cdad8d1c32e550fe07698ffd353d3137d
2242938055 <2242938055@qq.com>
2023-07-21 15:36:05 +0800

    /editor remove ‘elementui’

--- client/editor/src/main.js
+++ client/editor/src/main.js
@@ -1,13 +1,11 @@
 // css 样式
 import './assets/css/main.scss'
-import elementui from './elementui/index.js'
 import routes from './routes'
 import App from './App.vue'
 
 export const start = async () => {
 
   const Vue = window.Vue
-  Vue.use(elementui)
 
   const router = new window.nocode.VueRouter({
     mode: 'history',
