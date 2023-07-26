a031fc0fc072c6b418a5b972a5684e52b698a20d
2242938055 <2242938055@qq.com>
2023-07-26 16:47:17 +0800

    SetParams.vue edit

--- client/editor/src/views/editor/components/edit/SetParams.vue
+++ client/editor/src/views/editor/components/edit/SetParams.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-        <div class="param-item" v-for="(item, i) in value" :key="i">
-            <el-input size="mini" v-model="item.k" @input="onInput" />
-            <el-input size="mini" v-model="item.v" @input="onInput" />
-            <el-button size="mini" class="btn-remove" @click="onRemove(i)">-</el-button>
-        </div>
         <el-button size="mini" @click="onAdd">+</el-button>
+        <el-table :data="value" emptyText="no params now">
+            <el-table-column label="name">
+                <template slot-scope="scope">
+                    <el-input size="mini" v-model="scope.row.k" @input="onInput" />
+                </template>
+            </el-table-column>
+            <el-table-column label="source">
+                <template slot-scope="scope">
+                    <el-select v-model="scope.row.source">
+                        <el-option v-for="source in paramSources" :key="source" :label="source" :value="source" />
+                    </el-select>
+                </template>
+            </el-table-column>
+            <el-table-column label="value">
+                <template slot-scope="scope">
+                    <el-input size="mini" v-model="scope.row.v" @input="onInput" />
+                </template>
+            </el-table-column>
+            <el-table-column label="remove">
+                <template slot-scope="scope">
+                    <el-button size="mini" class="btn-remove" @click="onRemove(scope.row.i)">-</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -24,6 +43,11 @@ export default {
     data() {
         return {
         }
+    },
+    computed: {
+        paramSources() {
+            return ['value', 'com_data', 'com_ref', 'route_query', 'cmd_data']
+        },
     },
     mounted() {
         console.log('[SetParams] mounted', this.value)
@@ -44,15 +68,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-.param-item {
-    display: flex;
-    flex-direction: row;
-    line-height: 28px;
-
-    .btn-remove {
-        padding: 0;
-        width: 50px;
-    }
-}
-</style>
+<style lang="scss" scoped></style>
