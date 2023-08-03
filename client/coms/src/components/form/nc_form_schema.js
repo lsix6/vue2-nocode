f5bb96924e4ac7a81de8fc7e8c37cbbc29676a3c
2242938055 <2242938055@qq.com>
2023-08-03 13:49:45 +0800

    nc_form_schema.js add ‘com_methods’

--- client/coms/src/components/form/nc_form_schema.js
+++ client/coms/src/components/form/nc_form_schema.js
@@ -84,6 +84,10 @@ export default {
         },
         data_methods: {
             getData: {},
-        }
+        },
+        com_methods: {
+            commitData: {},
+            cancelFormChange: {},
+        },
     },
 };
