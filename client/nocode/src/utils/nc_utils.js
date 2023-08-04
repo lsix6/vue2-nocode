95744b62e32259328798524156c2010155abea7a
2242938055 <2242938055@qq.com>
2023-08-04 15:21:06 +0800

    nc_utils.js add ‘deepFreeze()’

--- client/nocode/src/utils/nc_utils.js
+++ client/nocode/src/utils/nc_utils.js
@@ -49,3 +49,19 @@ export const parseValue = (type, value) => {
     //
     return v
 }
+
+export function deepFreeze(obj) {
+    // 取回定义在obj上的属性名
+    const propNames = Object.getOwnPropertyNames(obj);
+
+    // 在冻结自身之前冻结属性
+    propNames.forEach((name) => {
+        const prop = obj[name];
+
+        // 如果prop是个对象，冻结它
+        if (typeof prop === 'object' && prop !== null) deepFreeze(prop);
+    });
+
+    // 冻结自身(no-op if already frozen)
+    return Object.freeze(obj);
+}
