6b6fb88acf0a3e7c1543742eb7d8c21612fa89e9
2242938055 <2242938055@qq.com>
2023-08-11 13:46:20 +0800

    /nocode remove ‘/pages’

--- client/nocode/src/init_nocode.js
+++ client/nocode/src/init_nocode.js
@@ -1,28 +1,6 @@
 
 import './utils/nc_msg_box.js'
-import { request_json } from './utils/nc_request'
-
-import { register_module } from './pages/base/pages'
-import { product_info } from './pages/product/product_info'
-import { user_info } from './pages/user/user_info'
-
 
 export const init_nocode = async () => {
-    
-    // console.log('***** module_info begin *****')
-    // console.log(JSON.stringify(user_info))
-    // console.log('***** module_info end *****')
-    
-    register_module(product_info)
-    register_module(user_info)
-    //
-    // await Promise.all([
-    //     request_json('/nocode/static/modules/product.json'),
-    //     request_json('/nocode/static/modules/user.json'),
-    // ]).then(modules => {
-    //     modules.forEach(moduleInfo => {
-    //         register_module(moduleInfo)
-    //     })
-    // })
 
 }
